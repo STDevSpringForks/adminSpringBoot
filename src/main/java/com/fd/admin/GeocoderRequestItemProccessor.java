9ed2cb60abacbe99cf851ed785878c21ca301de9
Froy <fdaf58@gmail.com>
2017-09-14 22:38:58 -0500

    comment spring-batch

--- src/main/java/com/fd/admin/GeocoderRequestItemProccessor.java
+++ src/main/java/com/fd/admin/GeocoderRequestItemProccessor.java
@@ -1,33 +1,33 @@
-package com.fd.admin;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.batch.item.ItemProcessor;
-
-import com.fd.admin.model.GeocoderRequest;
-
-public class GeocoderRequestItemProccessor implements ItemProcessor<GeocoderRequest,GeocoderRequest> {
-
-	 private static final Logger log = LoggerFactory.getLogger(GeocoderRequestItemProccessor.class);
-
-	    @Override
-	    public GeocoderRequest process(GeocoderRequest item) throws Exception {
-
-	        int idGeocoderRequest = item.getIdGeocoderRequest();
-	        
-	        String addressLine1 = item.getAddressLine1().toUpperCase();
-	        String addressLine2 = item.getAddressLine2().toUpperCase();
-	        String city = item.getCity().toUpperCase();
-	        String state = item.getState().toUpperCase();
-	        String zipCode = item.getZipCode().toUpperCase();
-	        String county = item.getCounty().toUpperCase();
-	        
-	        String latitude = item.getLatitude().toUpperCase();
-	        String longitude = item.getLongitude().toUpperCase();
-	        
-	        GeocoderRequest userProcess = new GeocoderRequest(idGeocoderRequest, addressLine1, addressLine2, city, state, zipCode, county, latitude, longitude);
-
-	        return userProcess;
-	    }
-	
-}
+//package com.fd.admin;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.batch.item.ItemProcessor;
+//
+//import com.fd.admin.model.GeocoderRequest;
+//
+//public class GeocoderRequestItemProccessor implements ItemProcessor<GeocoderRequest,GeocoderRequest> {
+//
+//	 private static final Logger log = LoggerFactory.getLogger(GeocoderRequestItemProccessor.class);
+//
+//	    @Override
+//	    public GeocoderRequest process(GeocoderRequest item) throws Exception {
+//
+//	        int idGeocoderRequest = item.getIdGeocoderRequest();
+//	        
+//	        String addressLine1 = item.getAddressLine1().toUpperCase();
+//	        String addressLine2 = item.getAddressLine2().toUpperCase();
+//	        String city = item.getCity().toUpperCase();
+//	        String state = item.getState().toUpperCase();
+//	        String zipCode = item.getZipCode().toUpperCase();
+//	        String county = item.getCounty().toUpperCase();
+//	        
+//	        String latitude = item.getLatitude().toUpperCase();
+//	        String longitude = item.getLongitude().toUpperCase();
+//	        
+//	        GeocoderRequest userProcess = new GeocoderRequest(idGeocoderRequest, addressLine1, addressLine2, city, state, zipCode, county, latitude, longitude);
+//
+//	        return userProcess;
+//	    }
+//	
+//}
