a20352fde9c06c2e42ea0ce9437c2185713b57b9
Froy <fdaf58@gmail.com>
2025-06-12 02:00:11 -0600

    Add Thymeleaf templates and configuration

--- src/main/resources/WebConfig.java
+++ src/main/resources/WebConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @EnableWebMvc
 @Configuration
@@ -12,15 +15,44 @@ public class WebConfig implements WebMvcConfigurer {
     private static final String RESOURCES_LOCATION = "/resources/";
     private static final String RESOURCES_HANDLER = RESOURCES_LOCATION + "**";
 
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML");
+        resolver.setCharacterEncoding("UTF-8");
+        return resolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(templateResolver());
+        return engine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver thymeleafViewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setOrder(1);
+        return resolver;
+    }
+
     /**
-     * Configure ViewResolvers to deliver preferred views using JSP.
+     * Configure ViewResolvers for Thymeleaf and fallback to JSP.
      */
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
-        registry.viewResolver(resolver);
+        registry.viewResolver(thymeleafViewResolver());
+
+        InternalResourceViewResolver jspResolver = new InternalResourceViewResolver();
+        jspResolver.setPrefix("/WEB-INF/views/");
+        jspResolver.setSuffix(".jsp");
+        jspResolver.setOrder(2);
+        registry.viewResolver(jspResolver);
     }
 
 
--- src/main/webapp/WEB-INF/views/pages/home.jsp
+++
@@ -1,9 +0,0 @@
-<%@ include file="/WEB-INF/views/includes/header.jsp" %>
-<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
-<img src="${pageContext.request.contextPath}/resources/images/spring-logo.png">
-
-<h1>Ejemplo de Spring Boot</h1>
-<p>${message}</p>
-<p>Comenzamos la reunión a las ${startMeeting}</p>
-
-<%@ include file="/WEB-INF/views/includes/footer.jsp" %>
